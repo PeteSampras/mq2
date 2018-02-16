30875e2f3a4f0cc957704449f3cbba811cb49d70
PeteSampras <PeteSampras.mmobugs@gmail.com>
2018-02-16 13:28:09 -1000

    Update MQ2Farm.cpp

--- MQ2Farm/MQ2Farm.cpp
+++ MQ2Farm/MQ2Farm.cpp
@@ -16,18 +16,20 @@ PreSetup("MQ2Farm");
 
 // place all prototypes in this region
 #pragma region Prototypes
-
+void CheckAlias();
+void FarmCommand(PSPAWNINFO pChar, PCHAR szLine);
 #pragma endregion Prototypes
 
 // Place all variables in this region
 #pragma region Variables
-bool activated = false;
-
+bool activated = false, bDebugging = false;
+char szFarmMob[MAX_STRING] = {0}, szMyTargetID[MAX_STRING] = {0};
+int iPullRange=0,iZRadius=0,iPulses=0,iPulseDelay=0;
 #pragma endregion Variables
 
 // place all initial settings in this region
 #pragma region Settings
-
+iPulseDelay=3;
 #pragma endregion Settings
 
 // place all Inline functions in this region
@@ -51,6 +53,16 @@ static inline BOOL WinState(CXWnd *Wnd)
 
 // place all utility functions in this region
 #pragma region UtilityFunctions
+void CheckAlias()
+{
+        char aliases[MAX_STRING] = "";
+        if (RemoveAlias("/ignorethis"))
+                strcat_s(aliases, " /ignorethis ");
+        if (RemoveAlias("/ignorethese"))
+                strcat_s(aliases, " /ignorethese ");
+        if (strlen(aliases) > 0)
+                WriteChatf("\ar[MQ2Farm]\ao::\ayWARNING\ao::\awAliases for \ao%s\aw were detected and temporarily removed.", aliases);
+}
 #pragma endregion UtilityFunctions
 
 
@@ -59,6 +71,14 @@ static inline BOOL WinState(CXWnd *Wnd)
 
 #pragma endregion PrimaryFunctions
 
+#pragma region Commands
+
+void FarmCommand(PSPAWNINFO pChar, PCHAR szLine)
+{
+        if (!InGameOK())
+                return;
+}
+#pragma endregion Commands
 
 // Call to activate plugin.
 // Add commands, aliases, datatypes, benchmarks, UI files, detours, etc.
@@ -67,7 +87,7 @@ void PluginOn()
 if(activated)
     return;
 activated=true;
-
+AddCommand("/farm", FarmCommand);
 
 }
 
@@ -84,72 +104,74 @@ activated=false;
 // Called once, when the plugin is to initialize
 PLUGIN_API VOID InitializePlugin(VOID)
 {
-	DebugSpewAlways("Initializing MQ2Farm");
+        DebugSpewAlways("Initializing MQ2Farm");
         if (InGameOK())
             PluginOn();
-	InitLib(PLUGIN_NAME);
+        InitLib(PLUGIN_NAME);
 }
 
 // Called once, when the plugin is to shutdown
 PLUGIN_API VOID ShutdownPlugin(VOID)
 {
-	DebugSpewAlways("Shutting down MQ2Farm");
-	PluginOff();
-	FreeLib(PLUGIN_NAME);
+        DebugSpewAlways("Shutting down MQ2Farm");
+        PluginOff();
+        FreeLib(PLUGIN_NAME);
 }
 
 // things you want to do when you begin zoning
-PLUGIN_API VOID OnBeginZone(VOID) 
-{ 
+PLUGIN_API VOID OnBeginZone(VOID)
+{
 }
 
 // things you want to do once you are done zoning
-PLUGIN_API VOID OnEndZone(VOID) 
-{ 
+PLUGIN_API VOID OnEndZone(VOID)
+{
 
-} 
+}
 
 // Called after entering a new zone
 PLUGIN_API VOID OnZoned(VOID)
 {
-	DebugSpewAlways("MQ2Farm::OnZoned()");
+        DebugSpewAlways("MQ2Farm::OnZoned()");
 }
 
 // Called once directly before shutdown of the new ui system, and also
 // every time the game calls CDisplay::CleanGameUI()
 PLUGIN_API VOID OnCleanUI(VOID)
 {
-	DebugSpewAlways("MQ2Farm::OnCleanUI()");
-	// destroy custom windows, etc
+        DebugSpewAlways("MQ2Farm::OnCleanUI()");
+        // destroy custom windows, etc
 }
 
 // Called once directly after the game ui is reloaded, after issuing /loadskin
 PLUGIN_API VOID OnReloadUI(VOID)
 {
-	DebugSpewAlways("MQ2Farm::OnReloadUI()");
-	// recreate custom windows, etc
+        DebugSpewAlways("MQ2Farm::OnReloadUI()");
+        // recreate custom windows, etc
 }
 
 // Called every frame that the "HUD" is drawn -- e.g. net status / packet loss bar
 PLUGIN_API VOID OnDrawHUD(VOID)
 {
-	// DONT leave in this debugspew, even if you leave in all the others
+        // DONT leave in this debugspew, even if you leave in all the others
     //DebugSpewAlways("MQ2Farm::OnDrawHUD()");
 }
 
 // Called once directly after initialization, and then every time the gamestate changes
 PLUGIN_API VOID SetGameState(DWORD GameState)
 {
-	DebugSpewAlways("MQ2Farm::SetGameState()");
+        DebugSpewAlways("MQ2Farm::SetGameState()");
     //if (GameState==GAMESTATE_INGAME)
-	// create custom windows if theyre not set up, etc
+        // create custom windows if theyre not set up, etc
 }
 
 
 // This is called every time MQ pulses
 PLUGIN_API VOID OnPulse(VOID)
 {
-
+if(++iPulses < iPulseDelay || !InGameOK())
+        return;
+iPulse=0;
 }
 
 // This is called every time WriteChatColor is called by MQ2Main or any plugin,
@@ -157,16 +179,16 @@ PLUGIN_API VOID OnPulse(VOID)
 // CALL CEverQuest::dsp_chat MAKE SURE TO IMPLEMENT EVENTS HERE (for chat plugins)
 PLUGIN_API DWORD OnWriteChatColor(PCHAR Line, DWORD Color, DWORD Filter)
 {
-	DebugSpewAlways("MQ2Farm::OnWriteChatColor(%s)",Line);
-	return 0;
+        DebugSpewAlways("MQ2Farm::OnWriteChatColor(%s)",Line);
+        return 0;
 }
 
 // This is called every time EQ shows a line of chat with CEverQuest::dsp_chat,
 // but after MQ filters and chat events are taken care of.
 PLUGIN_API DWORD OnIncomingChat(PCHAR Line, DWORD Color)
 {
-	DebugSpewAlways("MQ2Farm::OnIncomingChat(%s)",Line);
-	return 0;
+        DebugSpewAlways("MQ2Farm::OnIncomingChat(%s)",Line);
+        return 0;
 }
 
 // This is called each time a spawn is added to a zone (inserted into EQ's list of spawns),
@@ -174,14 +196,14 @@ PLUGIN_API DWORD OnIncomingChat(PCHAR Line, DWORD Color)
 // NOTE: When you zone, these will come BEFORE OnZoned
 PLUGIN_API VOID OnAddSpawn(PSPAWNINFO pNewSpawn)
 {
-	DebugSpewAlways("MQ2Farm::OnAddSpawn(%s)",pNewSpawn->Name);
+        DebugSpewAlways("MQ2Farm::OnAddSpawn(%s)",pNewSpawn->Name);
 }
 
 // This is called each time a spawn is removed from a zone (removed from EQ's list of spawns).
 // It is NOT called for each existing spawn when a plugin shuts down.
 PLUGIN_API VOID OnRemoveSpawn(PSPAWNINFO pSpawn)
 {
-	DebugSpewAlways("MQ2Farm::OnRemoveSpawn(%s)",pSpawn->Name);
+        DebugSpewAlways("MQ2Farm::OnRemoveSpawn(%s)",pSpawn->Name);
 }
 
 // This is called each time a ground item is added to a zone
@@ -189,12 +211,12 @@ PLUGIN_API VOID OnRemoveSpawn(PSPAWNINFO pSpawn)
 // NOTE: When you zone, these will come BEFORE OnZoned
 PLUGIN_API VOID OnAddGroundItem(PGROUNDITEM pNewGroundItem)
 {
-	DebugSpewAlways("MQ2Farm::OnAddGroundItem(%d)",pNewGroundItem->DropID);
+        DebugSpewAlways("MQ2Farm::OnAddGroundItem(%d)",pNewGroundItem->DropID);
 }
 
 // This is called each time a ground item is removed from a zone
 // It is NOT called for each existing ground item when a plugin shuts down.
 PLUGIN_API VOID OnRemoveGroundItem(PGROUNDITEM pGroundItem)
 {
-	DebugSpewAlways("MQ2Farm::OnRemoveGroundItem(%d)",pGroundItem->DropID);
+        DebugSpewAlways("MQ2Farm::OnRemoveGroundItem(%d)",pGroundItem->DropID);
 }
