67553d1a9f230abee18a4e30e1870b7e0b45669a
PeteSampras <PeteSampras.mmobugs@gmail.com>
2018-02-17 11:20:45 -1000

    Update MQ2Farm.cpp

--- MQ2Farm/MQ2Farm.cpp
+++ MQ2Farm/MQ2Farm.cpp
@@ -26,8 +26,10 @@ PreSetup("MQ2Farm");
 // place all prototypes in this region
 #pragma region Prototypes
 void CheckAlias();
+void Configure(char szCustomIni[MAX_STRING], int force);
 LONG Evaluate(PCHAR szLine);
 void FarmCommand(PSPAWNINFO pChar, PCHAR szLine);
+void FarmIniCommand(PSPAWNINFO pChar, PCHAR szLine);
 void IgnoreThisCommand(PSPAWNINFO pChar, PCHAR szLine);
 void IgnoreTheseCommand(PSPAWNINFO pChar, PCHAR szLine);
 void ListCommands();
@@ -47,6 +49,49 @@ vector<string> vFarmMobs, vIgnoreMobs;
 // place all initial settings in this region
 #pragma region Settings
 int iPulseDelay = 3;
+
+//ini entries
+DWORD MinLevel=0;
+DWORD MaxLevel=200;
+eSpawnType SpawnType;
+DWORD SpawnID;
+DWORD FromSpawnID;
+FLOAT Radius;
+CHAR szName[MAX_STRING];
+CHAR szBodyType[MAX_STRING];
+CHAR szRace[MAX_STRING];
+CHAR szClass[MAX_STRING];
+CHAR szLight[MAX_STRING];
+BOOL bSpawnID;
+BOOL bNotNearAlert;
+BOOL bNearAlert;
+BOOL bNoAlert;
+BOOL bAlert;
+BOOL bLight;
+BOOL bTargNext;
+BOOL bTargPrev;
+BOOL bXTarHater;
+BOOL bGM;
+BOOL bNamed;
+BOOL bMerchant=false;
+BOOL bAura;
+BOOL bBanner;
+BOOL bCampfire;
+DWORD NotNearAlertList;
+DWORD NearAlertList;
+DWORD NoAlertList;
+DWORD AlertList;
+DOUBLE ZRadius;
+DOUBLE FRadius;
+FLOAT xLoc;
+FLOAT yLoc;
+FLOAT zLoc;
+DWORD SortBy;
+BOOL bLoS;
+BOOL bExactName;
+BOOL bTargetable;
+DWORD PlayerState;
+
 #pragma endregion Settings
 
 // place all Inline functions in this region
@@ -103,24 +148,7 @@ void ListCommands()
     WriteChatf("\ar[MQ2Farm]\ao::\ay/ignorethese --- Will ignore all spawns with this targets clean name");
 }
 
-void LoadIgnoreCommand(PSPAWNINFO pChar, PCHAR szLine)
-{
-        if (!InGameOK())
-                return;
-        else
-                sprintf_s(ImmuneINIFileName, "%s\\macros\\Mob_Ignore_List.ini", gszINIPath);
-        if (!pZoneInfo)
-                return;
-        sprintf_s(IgnoreINISection, "Ignores.%s", ((PZONEINFO)pZoneInfo)->ShortName);
-        GetPrivateProfileString(IgnoreINISection, "Ignore", "|", IgnoreList, MAX_STRING, ImmuneINIFileName);
-        if (!strcmp(IgnoreList, "|"))
-        {
-                WritePrivateProfileString(IgnoreINISection, "Ignore", IgnoreList, ImmuneINIFileName);
-        }
-        CHAR szList[MAX_STRING];
-        sprintf_s(szList, "\arIgnores:\aw%s", IgnoreList);
 
-}
 
 #pragma endregion UtilityFunctions
 
@@ -172,12 +200,22 @@ DWORD SearchSpawns(char szIndex[MAX_STRING])
 #pragma endregion PrimaryFunctions
 
 #pragma region Commands
+void FarmIniCommand(PSPAWNINFO pChar, PCHAR szLine)
+{
+	if (strlen(szLine) != 0)
+	{
+		CHAR Arg1[MAX_STRING] = { 0 };
+		GetArg(Arg1, szLine, 1);
+		WriteChatf("Loading custom INI: %s", Arg1);
+		Configure(Arg1, 1);
+	}
+}
 
 void FarmCommand(PSPAWNINFO pChar, PCHAR szLine)
 {
     if (!InGameOK())
                 return;
-    char szTemp[MAX_STRING] = "";
+    char szTemp[MAX_STRING] = {0};
 	strcpy_s(szTemp, szLine);
     if (strlen(szLine)==0)
     {
@@ -211,6 +249,8 @@ void FarmCommand(PSPAWNINFO pChar, PCHAR szLine)
             str1.replace(str1.find(Arg1), 1, ""); //actually replace
             sprintf_s(szTemp, "%s", str1.c_str()); // change szLine back to original minus the number
         }
+		char szNull[MAX_STRING] = { 0 }; //REMINDER: Remove this once we redo command
+		Configure(szNull, 0);			 //REMINDER: Remove this once we redo command
         WriteChatf("Searching for %s in %d radius", szLine,iPullRange);
     }
 
@@ -329,6 +369,24 @@ void IgnoreTheseCommand(PSPAWNINFO pChar, PCHAR szLine)
     }
 }
 
+void LoadIgnoreCommand(PSPAWNINFO pChar, PCHAR szLine)
+{
+	if (!InGameOK())
+		return;
+	else
+		sprintf_s(ImmuneINIFileName, "%s\\macros\\Mob_Ignore_List.ini", gszINIPath);
+	if (!pZoneInfo)
+		return;
+	sprintf_s(IgnoreINISection, "Ignores.%s", ((PZONEINFO)pZoneInfo)->ShortName);
+	GetPrivateProfileString(IgnoreINISection, "Ignore", "|", IgnoreList, MAX_STRING, ImmuneINIFileName);
+	if (!strcmp(IgnoreList, "|"))
+	{
+		WritePrivateProfileString(IgnoreINISection, "Ignore", IgnoreList, ImmuneINIFileName);
+	}
+	CHAR szList[MAX_STRING];
+	sprintf_s(szList, "\arIgnores:\aw%s", IgnoreList);
+
+}
 #pragma endregion Commands
 
 // Call to activate plugin.
@@ -339,11 +397,14 @@ if(activated)
     return;
 activated=true;
 AddCommand("/farm", FarmCommand);
+AddCommand("/botini", FarmIniCommand);
 AddCommand("/ignorethis", IgnoreThisCommand);
 AddCommand("/ignorethese", IgnoreTheseCommand);
 AddCommand("/imob", IgnoreMobCommand);
 AddCommand("/loadignore", LoadIgnoreCommand);
 EzCommand("/loadignore");
+
+
 }
 
 // Call to deactivate plugin.
@@ -355,6 +416,7 @@ void PluginOff()
     activated=false;
     DebugSpewAlways("Shutting down MQ2Farm");
     RemoveCommand("/farm");
+	RemoveCommand("/farmini");
     RemoveCommand("/ignorethis");
     RemoveCommand("/ignorethese");
     RemoveCommand("/imob");
@@ -477,4 +539,186 @@ PLUGIN_API VOID OnAddGroundItem(PGROUNDITEM pNewGroundItem)
 PLUGIN_API VOID OnRemoveGroundItem(PGROUNDITEM pGroundItem)
 {
         DebugSpewAlways("MQ2Farm::OnRemoveGroundItem(%d)",pGroundItem->DropID);
+}
+
+void Configure(char szCustomIni[MAX_STRING], int force)
+{
+	if (!InGameOK())
+		return;
+	char tempINI[MAX_STRING] = { 0 };
+	long Class = GetCharInfo()->pSpawn->mActorClient.Class;
+	long Races = GetCharInfo2()->Race;
+	long Level = GetCharInfo2()->Level;
+	sprintf_s(INIFileName, "%s\\%s_%s.ini", gszINIPath, EQADDR_SERVERNAME, GetCharInfo()->Name);
+	sprintf_s(INISection, "%s_%d_%s_%s", PLUGIN_NAME, Level, pEverQuest->GetRaceDesc(Races), pEverQuest->GetClassDesc(Class));
+	//Shrouded = GetCharInfo2()->Shrouded; //this is used if you are shrouded or in an instance
+	//if (!Shrouded)
+	//	INISection[strlen(PLUGIN_NAME)] = 0;
+	strcpy_s(tempINI, INISection);
+	_strlwr_s(szCustomIni, MAX_STRING);
+	if (force && strlen(szCustomIni) > 1 && !strstr(szCustomIni, "default"))
+		sprintf_s(INISection, "%s_%s", tempINI, szCustomIni);
+	char szTemp[MAX_STRING] = { 0 };
+/*	eSpawnType SpawnType;
+	DWORD SpawnID;
+	DWORD FromSpawnID;
+	FLOAT Radius;
+	CHAR szName[MAX_STRING];
+	CHAR szBodyType[MAX_STRING];
+	CHAR szRace[MAX_STRING];
+	CHAR szClass[MAX_STRING];
+	CHAR szLight[MAX_STRING];
+	BOOL bSpawnID;
+	BOOL bNotNearAlert;
+	BOOL bNearAlert;
+	BOOL bLight;
+	BOOL bTargNext;
+	BOOL bTargPrev;
+	BOOL bXTarHater;
+	BOOL bGM;
+	BOOL bMerchant = false;
+	BOOL bAura;
+	BOOL bBanner;
+	BOOL bCampfire;
+	DWORD NotNearAlertList;
+	DWORD NearAlertList;
+	DWORD NoAlertList;
+	DWORD AlertList;
+	FLOAT xLoc;
+	FLOAT yLoc;
+	FLOAT zLoc;
+	DWORD SortBy;
+	BOOL bExactName;
+	BOOL bTargetable;
+	DWORD PlayerState;
+	*/
+	// DEBUG_DUMPFILE = 0 != GetPrivateProfileInt(INISection, "Debugging", 0, INIFileName);//log to file?
+	iPullRange = GetPrivateProfileInt(INISection, "PullRange", 1000, INIFileName); //default pull range.. but i think this is same as radius.. so.. rename it?
+	MinLevel = GetPrivateProfileInt(INISection, "MinLevel", 1, INIFileName); //default min level to search for
+	MaxLevel = GetPrivateProfileInt(INISection, "MaxLevel", 200, INIFileName); //default max level to search for
+	bLoS = GetPrivateProfileInt(INISection, "LineOfSight", 0, INIFileName); //do I require line of sight?
+	ZRadius = GetPrivateProfileInt(INISection, "ZRadius", 1000, INIFileName); // default Zradius
+	FRadius = GetPrivateProfileInt(INISection, "Radius", 1000, INIFileName); // Default radius
+	bNamed = GetPrivateProfileInt(INISection, "NamedMobs", 0, INIFileName);// default no
+	bNoAlert = GetPrivateProfileInt(INISection, "AvoidAlertMobs", 1, INIFileName);//default avoid alert mobs
+	bAlert = GetPrivateProfileInt(INISection, "FindAlertMobs", 0, INIFileName);//default avoid alert mobs
+
+	//examples:
+	/*
+	UseAENukes = GetPrivateProfileInt(INISection, "UseAENukes", 1, INIFileName);//use AE nukes?
+	UseAura = GetPrivateProfileInt(INISection, "UseAura", 1, INIFileName);//use auras?
+	AggroAt = GetPrivateProfileInt(INISection, "AggroAt", 70, INIFileName);//use aggro at %?
+	JoltAt = GetPrivateProfileInt(INISection, "JoltAt", 70, INIFileName);//use jolt at %?
+	if (MyClass == 1 || MyClass == 3 || MyClass == 4 || MyClass == 7 || MyClass == 8 || MyClass == 9 || MyClass == 15 || MyClass == 16)
+		MeleeAt = GetPrivateProfileInt(INISection, "MeleeAt", 100, INIFileName);//use melee at %?
+	else
+		MeleeAt = GetPrivateProfileInt(INISection, "MeleeAt", 0, INIFileName);//use melee at %?
+	AutofireAt = GetPrivateProfileInt(INISection, "AutofireAt", 0, INIFileName);//use autofire at %?
+	AutofireDistance = GetPrivateProfileInt(INISection, "AutofireDistance", 100, INIFileName);//use autofire at %?
+	ImHitAt = GetPrivateProfileInt(INISection, "ImHitAt", 50, INIFileName);//use melee at %?
+																		   //assist settings
+	AssistAt = GetPrivateProfileInt(INISection, "AssistAt", 100, INIFileName);//assist at
+	AssistRange = GetPrivateProfileInt(INISection, "AssistRange", 100, INIFileName);//assist range
+	UseManualAssist = GetPrivateProfileInt(INISection, "UseManualAssist", 0, INIFileName);// use manual /assist
+	GetPrivateProfileString(INISection, "AssistName", "", AssistName, sizeof(AssistName), INIFileName);
+	MercAssistAt = GetPrivateProfileInt(INISection, "MercAssistAt", 100, INIFileName);//merc assist at
+	MercAssistRange = GetPrivateProfileInt(INISection, "MercAssistRange", 0, INIFileName);//merc assist range
+	FaceAngle = GetPrivateProfileInt(INISection, "FaceAngle", 360, INIFileName);//Angle, if greater, to /face
+	DebuffsSpellFirst = GetPrivateProfileInt(INISection, "DebuffsSpellFirst", 0, INIFileName);//use spells before AA for debuffs section?
+	if (Class == 2 || Class == 6 || Class == 10 || Class == 11 || Class == 12 || Class == 13 || Class == 14)
+		RepositionIfSummoned = GetPrivateProfileInt(INISection, "RepositionIfSummoned", 1, INIFileName);//reposition if summoned?
+	else
+		RepositionIfSummoned = GetPrivateProfileInt(INISection, "RepositionIfSummoned", 0, INIFileName);//reposition if summoned?
+	GetPrivateProfileString(INISection, "RestrictedZones", ",344,202,151,345,", RestrictedZones, sizeof(RestrictedZones), INIFileName);
+	GetPrivateProfileString(INISection, "AutoFollowSettings", "30 healer", AutoFollowSettings, sizeof(AutoFollowSettings), INIFileName);
+	GetPrivateProfileString(INISection, "NetBotsName", "NULL", NetBotsName, sizeof(NetBotsName), INIFileName);
+	GetPrivateProfileString(INISection, "CustomPetSpell", "NULL", CustomPetSpell, sizeof(CustomPetSpell), INIFileName);
+	GetPrivateProfileString(INISection, "PetIllusionItem", "NULL", PetIllusionItem, sizeof(PetIllusionItem), INIFileName);
+	GetPrivateProfileString(INISection, "BuffPassword", "NULL", BuffPassword, sizeof(BuffPassword), INIFileName);
+	GetPrivateProfileString(INISection, "AuthorizedUsers", "|guild|group|netbots|", AuthorizedUsers, sizeof(AuthorizedUsers), INIFileName);
+	GetPrivateProfileString(INISection, "BotCommandsOn", "", BotCommandsOn, sizeof(BotCommandsOn), INIFileName);
+	GetPrivateProfileString(INISection, "BotCommandsOff", "", BotCommandsOff, sizeof(BotCommandsOff), INIFileName);
+	GetPrivateProfileString(INISection, "PullSkillName", "", PullSkill, sizeof(PullSkill), INIFileName);
+	GetPrivateProfileString(INISection, "AttackCommand", "/killthis", AttackCommand, sizeof(AttackCommand), INIFileName);
+	DoDebug = GetPrivateProfileInt(INISection, "DoDebug", 0, INIFileName);//debug?
+	PullingDistance = GetPrivateProfileInt(INISection, "PullingDistance", 0, INIFileName);//debug?
+	MQ2NavDistance = GetPrivateProfileInt(INISection, "MQ2NavDistance", 10, INIFileName);//debug?
+	AnnounceBehaviorChange = GetPrivateProfileInt(INISection, "AnnounceBehaviorChange", 0, INIFileName);//debug?
+	PetReagentCheck = GetPrivateProfileInt(INISection, "PetReagentCheck", 1, INIFileName);//Check for pet reagents before casting?
+	SafetyInvisOnWarp = GetPrivateProfileInt(INISection, "SafetyInvisOnWarp", 0, INIFileName);// invis on warp?
+	if (BardClass)
+	{
+		GetPrivateProfileString(INISection, "RestTwist", "", RestTwist, sizeof(RestTwist), INIFileName);
+		GetPrivateProfileString(INISection, "CombatTwist", "", CombatTwist, sizeof(CombatTwist), INIFileName);
+	}
+*/
+
+	//now we write to the ini using the values we have to ensure ini is always accurate and presented in the order we want
+	int MyClass = GetCharInfo()->pSpawn->mActorClient.Class;
+	/*
+	sprintf_s(szTemp, "%.4f", botVersion);
+	WritePrivateProfileString(INISection, "Debugging", DEBUG_DUMPFILE ? "1" : "0", INIFileName);
+	WritePrivateProfileString(INISection, "BotVersion", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "BotCommandsOn", BotCommandsOn, INIFileName);
+	WritePrivateProfileString(INISection, "BotCommandsOff", BotCommandsOff, INIFileName);
+	*/
+	_itoa_s(iPullRange, szTemp, 10);
+	WritePrivateProfileString(INISection, "PullRange", szTemp, INIFileName);
+	_itoa_s(MinLevel, szTemp, 10);
+	WritePrivateProfileString(INISection, "MinLevel", szTemp, INIFileName);
+	_itoa_s(MaxLevel, szTemp, 10);
+	WritePrivateProfileString(INISection, "MaxLevel", szTemp, INIFileName);
+	_itoa_s(bLoS, szTemp, 10);
+	WritePrivateProfileString(INISection, "LineOfSight", szTemp, INIFileName);
+	_itoa_s(ZRadius, szTemp, 10);
+	WritePrivateProfileString(INISection, "ZRadius", szTemp, INIFileName);
+	_itoa_s(FRadius, szTemp, 10);
+	WritePrivateProfileString(INISection, "Radius", szTemp, INIFileName);
+	_itoa_s(bNamed, szTemp, 10);
+	WritePrivateProfileString(INISection, "NamedMobs", szTemp, INIFileName);
+	_itoa_s(bNoAlert, szTemp, 10);
+	WritePrivateProfileString(INISection, "AvoidAlertMobs", szTemp, INIFileName);
+	_itoa_s(bAlert, szTemp, 10);
+	WritePrivateProfileString(INISection, "FindAlertMobs", szTemp, INIFileName);
+
+// examples that need changed:
+	/*
+	WritePrivateProfileString(INISection, "UseFightBuffs", szTemp, INIFileName);
+	_itoa_s(UseMainTankBuffs, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseMainTankBuffs", szTemp, INIFileName);
+	_itoa_s(UseManaInCombat, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseManaInCombat", szTemp, INIFileName);
+	_itoa_s(UseManaOnOthers, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseManaOnOthers", szTemp, INIFileName);
+	_itoa_s(UseMez, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseMez", szTemp, INIFileName);
+	_itoa_s(iUsePet, szTemp, 10);
+	WritePrivateProfileString(INISection, "UsePet", szTemp, INIFileName);
+	_itoa_s(PetReagentCheck, szTemp, 10);
+	WritePrivateProfileString(INISection, "PetReagentCheck", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "PetIllusionItem", PetIllusionItem, INIFileName);
+	WritePrivateProfileString(INISection, "CustomPetSpell", CustomPetSpell, INIFileName);
+	_itoa_s(UsePetBuffs, szTemp, 10);
+	WritePrivateProfileString(INISection, "UsePetBuffs", szTemp, INIFileName);
+	_itoa_s(StandOnPetAttack, szTemp, 10);
+	WritePrivateProfileString(INISection, "StandOnPetAttack", szTemp, INIFileName);
+	_itoa_s(UseSelfBuffs, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseSelfBuffs", szTemp, INIFileName);
+	_itoa_s(AutoFollowLeader, szTemp, 10);
+	WritePrivateProfileString(INISection, "AutoFollowLeader", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "AutoFollowSettings", AutoFollowSettings, INIFileName);
+	_itoa_s(UseNetBots, szTemp, 10);
+	WritePrivateProfileString(INISection, "UseNetBots", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "NetBotsName", NetBotsName, INIFileName);
+	_itoa_s(DoDebug, szTemp, 10);
+	WritePrivateProfileString(INISection, "DoDebug", szTemp, INIFileName);
+	_itoa_s(PullingDistance, szTemp, 10);
+	WritePrivateProfileString(INISection, "PullingDistance", szTemp, INIFileName);
+	_itoa_s(MQ2NavDistance, szTemp, 10);
+	WritePrivateProfileString(INISection, "MQ2NavDistance", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "PullSkillName", PullSkill, INIFileName);
+	_itoa_s(AnnounceBehaviorChange, szTemp, 10);
+	WritePrivateProfileString(INISection, "AnnounceBehaviorChange", szTemp, INIFileName);
+	WritePrivateProfileString(INISection, "AttackCommand", AttackCommand, INIFileName);
+	*/
 }
